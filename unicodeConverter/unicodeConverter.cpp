1c46041b6a34c519ca1c17ef78bffdf3c26cf67f
FroskyLT <kinglync1@gmail.com>
2020-10-21 23:37:03 +0300

    modified interface added menu, added new functions (hex_to_bin, bin_to_dec)

--- unicodeConverter/unicodeConverter.cpp
+++ unicodeConverter/unicodeConverter.cpp
@@ -4,6 +4,7 @@
 #include <fstream>
 #include <map>
 #include <string>
+#include <cmath>
 
 using namespace std;
 
@@ -17,29 +18,99 @@ void dec_to_bin(int dec, vector<char>& bin);
 void bin_to_hex(vector<char> bin, string& hex);
 void uni_to_utf8(vector<char> bin, int dec, string& utf);
 
+void hex_to_bin(vector<char>& bin, string hex);
+int bin_to_dec(vector<char> bin);
+
 int main()
 {
-	cout << "HTML-code -> UNICODE -> UTF-8\n\n";
-	Data data;
-	cin >> data.DEC;
-
-	dec_to_bin(data.DEC, data.BIN);
-	bin_to_hex(data.BIN, data.UNI);
-	uni_to_utf8(data.BIN, data.DEC, data.UTF8);
-
-	cout << "UNICODE: U+";
-	for (int i = data.UNI.size(); i < 4; i++) cout << "0";
-	cout << data.UNI << endl;
-	cout << "UTF-8: ";
-	for (int i = 0; i != data.UTF8.size(); i++) {
-		cout << data.UTF8[i];
-		if ((i + 1) % 2 == 0 && i + 1 != data.UTF8.size()) cout << " ";
-	}
-	/*for (int i = 0; i != BIN.size(); i++)
+	bool condition = true;
+	while (condition)
 	{
-		cout << BIN[i];
-		if ((i + 1) % 4 == 0) cout << " ";
-	}*/
+		int temp;
+		cout << "1. change DEC -> UTF-8" << endl;
+		cout << "2. convert 368intel.txt file from CP437 to UTF-8 and print it in new file" << endl;
+		cout << "3. exit" << endl;
+		cin >> temp;
+
+		if (temp == 1)
+		{
+			system("CLS");
+			cout << "HTML-code -> UNICODE -> UTF-8\n\n";
+			Data data;
+			cin >> data.DEC;
+
+			dec_to_bin(data.DEC, data.BIN);
+			bin_to_hex(data.BIN, data.UNI);
+			uni_to_utf8(data.BIN, data.DEC, data.UTF8);
+
+			ofstream fr("convert_result.txt");
+
+			cout << "UNICODE: U+"; fr << "UNICODE: U+";
+			for (int i = data.UNI.size(); i < 4; i++) fr << "0";
+			cout << data.UNI << endl; fr << data.UNI << endl;
+
+			cout << "UTF-8: "; fr << "UTF-8: ";
+			for (int i = 0; i != data.UTF8.size(); i++) {
+				cout << data.UTF8[i]; fr << data.UTF8[i];
+				if ((i + 1) % 2 == 0 && i + 1 != data.UTF8.size()) { cout << " "; fr << " "; }
+			}
+
+			cout << endl; fr << endl;
+			cout << "CHAR: " << char(data.DEC) << endl; fr << "CHAR: " << char(data.DEC);
+			cout << "You can check Your convert_result.txt\n\n";
+			fr.close();
+
+			cout << "Do you want to continue?\n";
+			cout << "1. Yes\n";
+			cout << "2. No\n";
+			cin >> temp;
+			if (temp != 1) condition = false;
+		}
+		else if (temp == 2)
+		{
+			ifstream fd("386intel.txt");
+			ofstream fr("output.txt");
+
+			Data data;
+			char currentSym;
+
+			map<int, string> CP437;
+			map<int, string>::iterator it;
+			int cpDec; string cpHex;
+
+			ifstream cp("CP437.txt");
+			while (cp >> cpHex >> cpDec) CP437[cpDec] = cpHex;
+			cp.close();
+
+			while (fd.get(currentSym))
+			{
+				data.DEC = currentSym;
+				if (data.DEC < 0)
+				{
+					data.DEC = 256 + data.DEC;
+					it = CP437.find(data.DEC);
+					if (it != CP437.end())
+						data.UNI = it->second;
+
+					hex_to_bin(data.BIN, data.UNI);
+					data.DEC = bin_to_dec(data.BIN);
+				}
+				else fr << currentSym;
+			}
+
+			cout << "done";
+			condition = false;
+			/* stringa konvertuot i baitu masyva
+			vektoriaus 1 narys veinas baitas
+			stringa issakidyt baitas, i koki vektoriu,
+			is baitu vektoriu jau konvertuojam i txt faila(is karto padarom charais elementus)
+			(char)Code[i] <- byte Code[3] = {0x00 ,0x01 , 0x83};*/
+		}
+		else {
+			condition = false;
+		}
+	}
+	return 0;
 }
 
 void dec_to_bin(int dec, vector<char>& bin)
@@ -59,6 +130,20 @@ void dec_to_bin(int dec, vector<char>& bin)
 	reverse(bin.begin(), bin.end());
 
 	//for (auto it : bin) cout << it;
+}
+
+int bin_to_dec(vector<char> bin)
+{
+	int dec = 0,j=0;
+
+	for (int i = bin.size() - 1; i >= 0; i--)
+	{
+		int temp = bin[i] - '0';
+		dec += (temp * pow(2, j));
+		j++;
+	}
+
+	return dec;
 }
 
 void bin_to_hex(vector<char> bin, string& hex)
@@ -92,6 +177,27 @@ void bin_to_hex(vector<char> bin, string& hex)
 	//for (auto it : hex) cout << it;
 }
 
+void hex_to_bin(vector<char>& bin, string hex)
+{
+	ifstream fd("bin-hex.txt");
+	map<char, string> bin_hex;
+	map<char, string>::iterator it;
+	string b; char h;
+	while (fd >> b >> h) bin_hex[h] = b;
+	fd.close();
+
+	bin = {};
+	for (int i = 0; i != hex.size(); i++)
+	{
+		it = bin_hex.find(hex[i]);
+		if (it != bin_hex.end())
+			for (int i = 0; i != it->second.size(); i++)
+				bin.push_back(it->second[i]);
+	}
+
+	//for (auto it : bin) cout << it;
+}
+
 void uni_to_utf8(vector<char> bin, int dec, string& utf)
 {
 	vector<char> utfbin;
@@ -112,7 +218,7 @@ void uni_to_utf8(vector<char> bin, int dec, string& utf)
 	else if (dec < 2048) { //2 bytes 
 		for (int i = 0; i < 16; i++)
 		{
-			if		(i == 6) utfbin.push_back('0');
+			if (i == 6) utfbin.push_back('0');
 			else if (i == 7) utfbin.push_back('1');
 			else if (i == 13) utfbin.push_back('0');
 			else if (i == 14) utfbin.push_back('1');
@@ -129,7 +235,7 @@ void uni_to_utf8(vector<char> bin, int dec, string& utf)
 	else if (dec < 65536) { //3 bytes 
 		for (int i = 0; i < 24; i++)
 		{
-			if		(i == 6) utfbin.push_back('0');
+			if (i == 6) utfbin.push_back('0');
 			else if (i == 7) utfbin.push_back('1');
 			else if (i == 14) utfbin.push_back('0');
 			else if (i == 15) utfbin.push_back('1');
@@ -150,7 +256,7 @@ void uni_to_utf8(vector<char> bin, int dec, string& utf)
 	else if (dec < 1114112) { //4 bytes
 		for (int i = 0; i < 32; i++)
 		{
-			if		(i == 6) utfbin.push_back('0');
+			if (i == 6) utfbin.push_back('0');
 			else if (i == 7) utfbin.push_back('1');
 			else if (i == 14) utfbin.push_back('0');
 			else if (i == 15) utfbin.push_back('1');
